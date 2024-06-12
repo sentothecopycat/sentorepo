fccee09b02d9cded35af9bcdd462e999fff8e8e3
sentothecopycat <sotentothecopycat@gmail.com>
2024-06-12 13:35:29 +0100

    now it works :)

--- x86_64/repo.sh
+++ x86_64/repo.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #.tar.gz
 repo-add -n -R sentorepo.db.tar.gz *.pkg.tar.zst
-
-sudo rm sentorepo.db sentorepo.files
-
-sudo mv sentorepo.files.tar.gz sentorepo.files
-
-sudo mv sentorepo.db.tar.gz sentorepo.db
